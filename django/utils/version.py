a631140463595c6546b60578b4e73e7166599821
Vitan Ivanov <vitan.ivanov@hyperscience.com>
2023-02-17 18:40:18 +0000

    Changing version from final to +hs

--- django/utils/version.py
+++ django/utils/version.py
@@ -37,7 +37,7 @@ def get_version(version=None):
             sub = ".dev%s" % git_changeset
 
     elif version[3] != 'final':
-        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'rc', 'post': 'post'}
+        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'rc', 'post': 'post', '+hs': '+hs'}
         sub = mapping[version[3]] + str(version[4])
 
     return main + sub
@@ -59,7 +59,7 @@ def get_complete_version(version=None):
         from django import VERSION as version
     else:
         assert len(version) == 5
-        assert version[3] in ('alpha', 'beta', 'rc', 'final', 'post')
+        assert version[3] in ('alpha', 'beta', 'rc', 'final', 'post', '+hs')
 
     return version
 
